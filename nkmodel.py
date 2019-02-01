50cd79176c12c87f522d2d51ca4545db26ef2348
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2019-01-31 22:21:30 -0800

    made the code look good

--- nkmodel.py
+++ nkmodel.py
@@ -1,5 +1,6 @@
 import random
 import sys
+import argparse
 
 def step(nodes):
     new_nodes = []
@@ -14,23 +15,33 @@ def step(nodes):
 def print_nodes(nodes):
     print(''.join(['1' if n[0] else '0' for n in nodes]))
 
-N = int(input("N: "))
-K = int(input("K: "))
-
-if N < K:
-    print('K can\'t be bigger than N!')
-    sys.exit()
-
-steps = int(input("Steps: "))
-
-nodes = []
-
-for i in range(0, N):
-    nodes.append((bool(random.getrandbits(1)), random.sample([x for x in range(N) if x != i], K), {}))
-
-print('start: ', end='')
-print_nodes(nodes)
-for i in range(steps):
-    nodes = step(nodes)
-    print(str(i + 1) + ': ', end='')
+def step_loop(nodes, steps, print_every=1):
+    print('start: ', end='')
     print_nodes(nodes)
+    for i in range(steps):
+        nodes = step(nodes)
+        if i % print_every == 0:
+            print(str(i + 1) + ': ', end='')
+            print_nodes(nodes)
+
+def main():
+    parser = argparse.ArgumentParser(description='NK Model Parameter handler')
+    parser.add_argument('-N', dest="N", type=int, required=True)
+    parser.add_argument('-K', dest="K", type=int, required=True)    
+    parser.add_argument('--steps', '-s', dest="steps", type=int, required=True)
+    parser.add_argument('--print-every', '-p', dest='print_every', type=int, default=1)
+    args = parser.parse_args()
+    N = args.N
+    K = args.K
+    if N < K:
+        parser.error("N must be greater than K")
+    steps = args.steps
+    print_every = args.print_every
+
+    nodes = []
+    for i in range(0, N):
+        nodes.append((bool(random.getrandbits(1)), random.sample([x for x in range(N) if x != i], K), {}))
+
+    step_loop(nodes, steps, print_every)
+
+main()
